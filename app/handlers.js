99985f5999ea1f921fa17fba189d902472d507cd
Joao Telo <joaotelo@Desenvs-MacBook-Air.local>
2017-09-21 11:15:40 -0300

    buy ticket not working yet

--- app/handlers.js
+++ app/handlers.js
@@ -2,6 +2,16 @@ const fromWei = n => n/1000000000000000000
 const moment = require('moment')
 
 module.exports = (bot, api = {}) => {
+    bot.on("/help", async msg => {
+        msg.reply.text(
+            `Available commands:\n` +
+            `/balance <address> - Displays the account balance for the address\n` +
+            `/tickets <address> - Displays lottery tickets for the address\n` +
+            `/lotteries - Shows available lottery information\n` +
+            `/buy <privatekey> <numbers> - Buy a ticket for the current lottery Example: /buy 0xPr1V4t3K3y 1,2,3,4`
+        )
+    })
+
     bot.on(/^\/balance (.+)$/, async (msg, props) => {
         const address = props.match[1];
         if(address.length !== 42) return msg.reply.text("Invalid address.")
@@ -38,5 +48,21 @@ module.exports = (bot, api = {}) => {
             `  Next drawing: ${moment.unix(lotto.nextDrawingDate).format('lll')}`
         }).join("\n")
         msg.reply.text(reply)
+    })
+
+    bot.on(/^\/buy (.+)$/, async (msg, props) => {
+        let inputs = props.match[1].split(' ')
+        let privateKey = inputs[0]
+        let numbers = inputs[1].split(',')
+
+        let result = ''
+        msg.reply.text("Thank you for your purchase! Your ticket is being processed ...")
+        try {
+            result = await api.buyTicket({ numbers, privateKey, ticektPrice: "10000000000000000", contractAddress: "0x7b501bbd3b3e5d8cae9a56fe5459e16e9dadb439"})
+        } catch(e) {
+            result = e.message
+        }
+        console.log('buy', result)
+        msg.reply.text(result)
     })
 }
